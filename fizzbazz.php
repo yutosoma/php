8fbb01a4a54c49dc5e19c86f37f3cc59116ef3f9
yutosoma <batteryy@outlook.jp>
2023-01-12 21:58:22 +0900

    second commit

--- fizzbazz.php
+++ fizzbazz.php
@@ -1,11 +1,11 @@
 <?php
     for($i= 1; $i <= 100; $i++){
-    if($i % 3 == 0){
-        echo "Fizz\n";
+    if ($i % 3 == 0 && $i % 5 == 0){
+        echo "FizzBuzz\n";
     } elseif($i % 5 == 0){
         echo "Buzz\n";
-    } elseif($i % 3 == 0 && $i % 5 == 0){
-        echo "FizzBuzz\n";
+    } elseif($i % 3 == 0){ 
+        echo "Fizz\n";
     } else {
         echo "$i.\n";
     }
