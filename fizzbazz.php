51043a4ced7a70176dc36662a48f5bfc55bce484
yutosoma <batteryy@outlook.jp>
2023-01-12 21:34:32 +0900

    first commit

---
+++ fizzbazz.php
@@ -0,0 +1,13 @@
+<?php
+    for($i= 1; $i <= 100; $i++){
+    if($i % 3 == 0){
+        echo "Fizz\n";
+    } elseif($i % 5 == 0){
+        echo "Buzz\n";
+    } elseif($i % 3 == 0 && $i % 5 == 0){
+        echo "FizzBuzz\n";
+    } else {
+        echo "$i.\n";
+    }
+   }
+        
