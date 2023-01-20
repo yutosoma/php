4c55eb46d3fc0ec5b096b6109baa88cb37f9006e
yutosoma <batteryy@outlook.jp>
2023-01-20 22:25:02 +0900

    third commit

---
+++ fizzbuzz.php
@@ -0,0 +1,15 @@
+<?php
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 3 == 0 && $i % 5 == 0) {
+        echo "FizzBuzz\n";
+
+    } elseif ($i % 5 == 0) {
+        echo "Buzz\n";
+
+    } elseif ($i % 3 == 0) {
+        echo "Fizz\n";
+
+    } else {
+        echo "$i.\n";
+    }
+}
